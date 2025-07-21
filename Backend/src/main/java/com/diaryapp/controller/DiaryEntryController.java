e3317252d7e17b21ec1c7c60125670c95f760d87
WMadaraChamudini <it23292154@my.sliit.lk>
2025-07-21 23:24:53 +0530

    access any port

--- Backend/src/main/java/com/diaryapp/controller/DiaryEntryController.java
+++ Backend/src/main/java/com/diaryapp/controller/DiaryEntryController.java
@@ -28,7 +28,7 @@ import com.diaryapp.repository.DiaryEntryRepository;
 
 @RestController
 @RequestMapping("/api/diary")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8000", "http://localhost:5500"})
+@CrossOrigin(origins = "*")
 public class DiaryEntryController {
     private final DiaryEntryRepository diaryEntryRepository;
 
